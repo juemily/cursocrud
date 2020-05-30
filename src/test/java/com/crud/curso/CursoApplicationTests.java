ece2472e9f62f00cf557d6803b680344e8dbee2f
Juemily <maluenga.juemily@gmail.com>
2020-05-30 10:13:46 -0400

    primer commit

---
+++ src/test/java/com/crud/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
